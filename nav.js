9062cf2e2e2b26a9e3fb5728e53800a929d9a2bf
Dustin <gabrielportes.dg@gmail.com>
2023-10-12 23:39:34 +0800

    Added a universal footer that can be reusable and edited some pictures of the catalogs

--- nav.js
+++ nav.js
@@ -6,7 +6,7 @@ const createNav = () => {
             <img src="img/dark-logo.png" class="brand-logo" alt="">
             <div class="nav-items">
                 <div class="search">
-                    <input type="text" class="search-box" placeholder="search brand, product">
+                    <input type="text" class="search-box" placeholder="search product">
                     <button class="search-btn">search</button>
                 </div>
                 <a href="#"><img src="img/user.png" alt=""></a>
@@ -15,10 +15,10 @@ const createNav = () => {
         </div>
         <ul class="links-container">
             <li class="link-item"><a href="#" class="link">home</a></li>
-            <li class="link-item"><a href="#" class="link">women</a></li>
-            <li class="link-item"><a href="#" class="link">men</a></li>
-            <li class="link-item"><a href="#" class="link">kids</a></li>
-            <li class="link-item"><a href="#" class="link">accessories</a></li>
+            <li class="link-item"><a href="#" class="link">Anniversary</a></li>
+            <li class="link-item"><a href="#" class="link">birthdays</a></li>
+            <li class="link-item"><a href="#" class="link">Plants</a></li>
+            <li class="link-item"><a href="#" class="link">Gift set</a></li>
         </ul>
     `;
     
