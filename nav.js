1e0bcf9a1f2c8846b9bb5059f762575fb3305ac1
Dustin <gabrielportes.dg@gmail.com>
2023-10-14 01:37:18 +0800

    added 404 page and search page and modified some line on footer and nav to redirect to a certain page(not complete)

--- nav.js
+++ nav.js
@@ -6,15 +6,15 @@ const createNav = () => {
             <img src="img/dark-logo.png" class="brand-logo" alt="">
             <div class="nav-items">
                 <div class="search">
-                    <input type="text" class="search-box" placeholder="search product">
-                    <button class="search-btn">search</button>
+                    <input type="text" class="search-box" placeholder="search product"> 
+                    <a href="search.html"><input type="submit" class="search-btn" value="Search"></a>
                 </div>
                 <a href="#"><img src="img/user.png" alt=""></a>
                 <a href="#"><img src="img/cart.png" alt=""></a>
             </div>
         </div>
         <ul class="links-container">
-            <li class="link-item"><a href="#" class="link">home</a></li>
+            <li class="link-item"><a href="index.html" class="link">home</a></li>
             <li class="link-item"><a href="#" class="link">Anniversary</a></li>
             <li class="link-item"><a href="#" class="link">birthdays</a></li>
             <li class="link-item"><a href="#" class="link">Plants</a></li>
