829e1d1c62a8b25583b3dd4aeb1efbe7092cc8ab
Dustin <gabrielportes.dg@gmail.com>
2023-11-13 01:48:41 +0800

    added function to search box but not working

--- js-files/nav.js
+++ js-files/nav.js
@@ -6,10 +6,19 @@ const createNav = () => {
             <img src="img/dark-logo.png" class="brand-logo" alt="">
             <div class="nav-items">
                 <div class="search">
-                    <input type="search" class="search-box" placeholder="search product" onkeyup="search()"> 
+                    <input type="text" class="search-box" placeholder="search product" onkeyup="myFunction()"> 
                     <a href="search.html"><input type="submit" class="search-btn" value="Search"></a>
-                </div>
-                <a href="#"><img src="img/user.png" alt=""></a>
+                    <ul id="myUL">
+                        <li><a href="https://www.twitch.tv/jaunueldota2">Adele</a></li>
+                        <li><a href="#">Agnes</a></li>
+                        <li><a href="#">Billy</a></li>
+                        <li><a href="#">Bob</a></li>
+                        <li><a href="#">Calvin</a></li>
+                        <li><a href="#">Christina</a></li>
+                        <li><a href="#">Cindy</a></li>
+                    </ul>
+                    </div>
+                <a href="login.html"><img src="img/user.png" alt=""></a>
                 <a href="cart.html"><img src="img/cart.png" alt=""></a>
             </div>
         </div>
@@ -29,6 +38,26 @@ const createNav = () => {
             <li class="link-item"><a href="aboutus.html" class="link">About Us</a></li>
         </ul>
     `;
-    
+
+    function myFunction() {
+        // Declare variables
+        var input, filter, ul, li, a, i, txtValue;
+        input = document.getElementsByClassName('search-box');
+        filter = input.value.toUpperCase();
+        ul = document.getElementById("myUL");
+        li = ul.getElementsByTagName('li');
+      
+        // Loop through all list items, and hide those who don't match the search query
+        for (i = 0; i < li.length; i++) {
+          a = li[i].getElementsByTagName("a")[0];
+          txtValue = a.textContent || a.innerText;
+          if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+          } else {
+            li[i].style.display = "none";
+          }
+        }
+      }
 }
 createNav();
+
