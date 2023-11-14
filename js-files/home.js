78e96022fe5e66b0f240ca08337dcd0b04730114
moshozzy <moshozzy17@gmail.com>
2023-11-14 19:12:34 +0800

    added array of products in home.js

--- js-files/home.js
+++ js-files/home.js
@@ -1,8 +1,78 @@
+const flowerData = [
+    {
+        "id": 1,
+        "name": "Roses",
+        "price": 2000,
+        "image": "img/redrose.png"
+    },
+    {
+        "id": 2,
+        "name": "Sunflower",
+        "price": 2220,
+        "image": "img/sunflower.png"
+    },
+    {
+        "id": 3,
+        "name": "Product 3",
+        "price": 250,
+        "image": "images/3.webp"
+    }
+    ,
+    {
+        "id": 4,
+        "name": "Product 4",
+        "price": 420,
+        "image": "images/4.webp"
+    },
+    {
+        "id": 5,
+        "name": "Product 5",
+        "price": 120,
+        "image": "images/5.webp"
+    },
+    {
+        "id": 6,
+        "name": "Product 6",
+        "price": 120,
+        "image": "images/6.webp"
+    }
+]
+
 const productContainers = [...document.querySelectorAll('.product-container')]
 const nxtBtn = [...document.querySelectorAll('.nxt-btn')]
 const preBtn = [...document.querySelectorAll('.pre-btn')]
 
-productContainers.forEach((item, i) =>{
+// Function to render flower data
+function renderFlowers() {
+    // Target the flower-card div
+    const flowerContainer = document.getElementById('flower-card');
+
+    // Check if the container exists
+    if (flowerContainer) {
+        // Map through the JSON data and create HTML for each flower item
+        const flowerHTML = flowerData.map((flower) => `
+            <div class="product-card">
+                <div class="product-image">
+                    <a href="product-${flower.id}.html">
+                        <img src="${flower.image}" class="product-thumb" alt="" />
+                    </a>
+                    <button class="card-btn">Add to Cart</button>
+                </div>
+                <div class="product-info">
+                    <h2>${flower.name}</h2>
+                    <span class="price">₱${flower.price}</span>
+                </div>
+            </div>
+            `
+        ).join('');
+
+        // Set the inner HTML of the flower-card div with the generated HTML
+        flowerContainer.innerHTML = flowerHTML;
+    }
+}
+
+
+productContainers.forEach((item, i) => {
     let containerDimensions = item.getBoundingClientRect();
     let containerWidth = containerDimensions.width;
 
@@ -14,3 +84,9 @@ productContainers.forEach((item, i) =>{
         item.scrollLeft -= containerWidth;
     })
 })
+
+// Call the renderFlowers function to render the flowers on page load
+document.addEventListener('DOMContentLoaded', function () {
+    console.log("panget si dacio")
+    renderFlowers()
+});
--- js-files/nav.js
+++ js-files/nav.js
@@ -85,7 +85,45 @@ function createNav() {
                     </ul>
                 </div>
                 <a href="login.html"><img src="img/user.png" alt=""></a>
-                <a href="cart.html"><img src="img/cart.png" alt=""></a>
+                <div class="iconCart">
+                    <img src="img/cart.png">
+                <div class="totalQuantity">0</div>
+                </div>
+
+                <div class="cart">
+                <h2>
+                    CART
+                </h2>
+        
+                <div class="listCart">
+        
+        
+                    <div class="item">
+                        <img src="img/bonsai.png">
+                        <div class="content">
+                            <div class="name">CoPilot / Black / Automatic</div>
+                            <div class="price">$550 / 1 product</div>
+                        </div>
+                        <div class="quantity">
+                            <button>-</button>
+                            <span class="value">3</span>
+                            <button>+</button>
+                        </div>
+                    </div>
+        
+        
+                </div>
+        
+                <div class="buttons">
+                    <div class="close">
+                        CLOSE
+                    </div>
+                    <div class="checkout">
+                        <a href="checkout.html">CHECKOUT</a>
+                    </div>
+                </div>
+            </div>
+
             </div>
         </div>
         
