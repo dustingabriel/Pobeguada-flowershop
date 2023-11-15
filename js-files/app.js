c22e54d2b400d2058681566d13bf3eb6dca7b1ac
moshozzy <moshozzy17@gmail.com>
2023-11-15 20:26:05 +0800

    fixed inc/dec and updated products

--- js-files/app.js
+++ js-files/app.js
@@ -76,24 +76,34 @@ function createCartItem(selectedFlower, flowerId, quantity) {
   return cartItem;
 }
 
-function incrementQuantity(cartItem) {
+function incrementQuantity(flowerId) {
+  const cartItem = Array.from(cartContainer.children).find(
+      (item) => item.dataset.flowerId === String(flowerId)
+  );
   const quantityElement = cartItem.querySelector(".quantity span.value");
   const quantity = parseInt(quantityElement.textContent, 10) + 1;
   quantityElement.textContent = quantity;
+
+  updateTotalQuantity();
+  saveCart();
 }
 
-function decrementQuantity(cartItem) {
+function decrementQuantity(flowerId) {
+  const cartItem = Array.from(cartContainer.children).find(
+      (item) => item.dataset.flowerId === String(flowerId)
+  );
   const quantityElement = cartItem.querySelector(".quantity span.value");
   let quantity = parseInt(quantityElement.textContent, 10);
 
   if (quantity > 1) {
-    quantity--;
-    quantityElement.textContent = quantity;
+      quantity--;
+      quantityElement.textContent = quantity;
   } else {
-    cartContainer.removeChild(cartItem);
+      cartContainer.removeChild(cartItem);
   }
 
   updateTotalQuantity();
+  saveCart();
 }
 
 function updateTotalQuantity() {
